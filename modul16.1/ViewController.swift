4ad911096686eb430d059295267b897976a48734
Bakastov Andrey <andrebakastov@mail.ru>
2024-07-30 19:54:06 +0300

    test1

--- modul16.1/ViewController.swift
+++ modul16.1/ViewController.swift
@@ -11,9 +11,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        hello()
     }
-
-
+    
+    private func hello(){
+        print ("hello world")
+    }
+    
 }
 
